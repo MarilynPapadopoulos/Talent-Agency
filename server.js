3509f520c93c4d85e5c34e13892cc08f14cfa7f3
Stevtm <steventmaddison@outlook.com>
2021-05-26 20:27:30 -0400

    install dependencies and set up server.js

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+// if we need to use helpers
+// const helpers = require("./utils/helpers")
+
+// initialize the app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// --- uncomment when connection.js is set up ---
+// const sequelize = require("./config/connection");
+
+// --- uncomment when sessions are set up ---
+// const SequelizeStore = require("connect-session-sequelize")(session.Store)
+
+// --- uncomment when sessions are set up ---
+// set up sessions
+// const sess = {
+// 	secret: "4g4JCTF3",
+// 	cookie: {},
+// 	resave: false,
+// 	saveUnititialized: true,
+// 	store: new SequelizeStore({
+// 		db: sequelize,
+// 	}),
+// };
+
+// app.use(session(sess));
+
+// --- uncomment when handlebars is set up ---
+// require express-handlebars
+// const hbs = exphbs.create({ helpers });
+
+// set up express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// --- uncomment when handlebars is set up ---
+// set up express handlebars
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
+
+app.use(require("./controllers/"));
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log("Now listening"));
+});
