c9f80b57b44f484bb4bf0b279b4508e258ed2c34
Stevtm <steventmaddison@outlook.com>
2021-05-27 21:17:24 -0400

    add GET routes to return all talent and one user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,10 +10,12 @@ router.get("/", (req, res) => {
 		attributes: { exclude: ["password"] },
 		include: [
 			{
+				// include the role name
 				model: Role,
 				attributes: ["role_name"],
 			},
 			{
+				// include all profile details - if the user is an agent, it will be null
 				model: Profile,
 				attributes: [
 					"gender",
@@ -40,7 +42,76 @@ router.get("/", (req, res) => {
 		});
 });
 
-// GET all talent= - /api/users/talent
-// this will be used on the agent landing page to populate the page with all talent prior to filtering
+// GET all talent - /api/users/talent
+// this will be used on the agent landing page to populate the page with all talent
+router.get("/talent", async (req, res) => {
+	// get the id for "talent" in the Roles table
+	let talent_id;
+
+	await Role.findOne({
+		where: { role_name: "talent" },
+	})
+		.then((dbRoleData) => {
+			talent_id = dbRoleData.id;
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+
+	// access User model and run .findAll() method
+	User.findAll({
+		attributes: { exclude: ["password"] },
+		where: {
+			role_id: talent_id,
+		},
+		include: [
+			{
+				// include the role name
+				model: Role,
+				attributes: ["role_name"],
+			},
+			{
+				// include all profile details - if the user is an agent, it will be null
+				model: Profile,
+				attributes: [
+					"gender",
+					"age",
+					"height",
+					"weight",
+					"eye_colour",
+					"hair_colour",
+					"size",
+					"complexion",
+					"speak_french",
+					"speak_spanish",
+					"speak_italian",
+					"speak_mandarin",
+					"skills",
+				],
+			},
+		],
+	})
+		.then((dbUserData) => res.status(200).json(dbUserData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+// GET one user by id - /api/users/:id
+router.get("/:id", (req, res) => {
+	User.findOne({
+		attributes: { exclude: ["password"] },
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbUserData) => res.status(200).json(dbUserData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
 
 module.exports = router;
